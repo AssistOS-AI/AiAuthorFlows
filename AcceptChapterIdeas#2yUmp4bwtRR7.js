be6e9d1b1026dc689893c3bde669aef61262b03f
alupoaiemircea <mircea@axiologic.net>
2024-02-20 09:40:47 +0200

    fix dependencies in flows

--- AcceptChapterIdeas#2yUmp4bwtRR7.js
+++ AcceptChapterIdeas#2yUmp4bwtRR7.js
@@ -1,16 +1,14 @@
 export class AcceptChapterIdeas {
     static id = "2yUmp4bwtRR7";
     static description = "Replaces the current main ideas with the generated ones";
-    static dependencies = ["sanitize"];
-    constructor(sanitize) {
-        this.sanitize = sanitize;
+    constructor() {
     }
 
     async start(documentId, chapterId, ideas) {
         try {
             let document = webSkel.currentUser.space.getDocument(documentId);
             let chapter = document.getChapter(chapterId);
-            await chapter.setMainIdeas(ideas.map(chapterIdea => this.sanitize(chapterIdea)));
+            await chapter.setMainIdeas(ideas.map(chapterIdea => webSkel.sanitize(chapterIdea)));
             await documentFactory.updateDocument(webSkel.currentUser.space.id, document);
             this.return(ideas);
         } catch (e) {
--- AcceptDocumentIdeas#3zauf2EYjh7n.js
+++ AcceptDocumentIdeas#3zauf2EYjh7n.js
@@ -3,12 +3,13 @@ export class AcceptDocumentIdeas {
     static description = "Replaces the current main ideas with the generated ones";
 
     constructor() {
+
     }
 
     async start(documentId, ideas) {
         try {
             let document = webSkel.currentUser.space.getDocument(documentId);
-            await document.setMainIdeas(ideas.map((documentIdea) => sanitize(documentIdea)));
+            await document.setMainIdeas(ideas.map((documentIdea) => webSkel.sanitize(documentIdea)));
             await documentFactory.updateDocument(webSkel.currentUser.space.id, document);
             this.return(ideas);
         } catch (e) {
