6f0ed532b10e1c70fd45d97125df0c927e58c03f
alupoaiemircea <mircea@axiologic.net>
2024-03-06 11:24:47 +0200

    fix flow

--- AddAlternativeChapter#34rPf47UN8va.js
+++ AddAlternativeChapter#34rPf47UN8va.js
@@ -2,16 +2,16 @@ export class AddAlternativeChapter {
 
    static id = "34rPf47UN8va" 
    static description = "Adds an alternative chapter";
+    static dependencies = ["Chapter"];
 
-   constructor(dependencies) {
-       const { Chapter } = dependencies;
+   constructor(Chapter) {
        this.Chapter = Chapter;
    }
    async start(documentId, chapterId, alternativeChapter){
        let document = webSkel.currentUser.space.getDocument(documentId);
        let chapter = document.getChapter(chapterId)
        alternativeChapter.id=webSkel.appServices.generateId();
-       for(let paragraph of chapterObj.paragraphs){
+       for(let paragraph of alternativeChapter.paragraphs){
            paragraph.id=webSkel.appServices.generateId();
        }
        chapter.alternativeChapters.push(new this.Chapter(alternativeChapter));
